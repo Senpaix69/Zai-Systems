1745a6a427100a613885d3ce8a99803898ff597c
Babar1998 <babararifmasood98@gmail.com>
2023-09-26 22:22:30 +0500

    Banners Updated

--- lib/consts/images.dart
+++ lib/consts/images.dart
@@ -124,7 +124,7 @@ const imgBusinessIntelligence =
 const imgBespoke =
     "assets/images/services/Bespoke IT Solutions for Business.png";
 const imgDataAnalytics = "assets/images/services/Data Analytics.png";
-const imgDevOps = "assets/images/services/Dev Ops.png";
+const imgDevOps = "assets/images/services/DevOps.png";
 const imgEcommerce = "assets/images/services/E-Commerce.png";
 const imgInformation = "assets/images/services/Information Strategy.png";
 const imgManagedCloud = "assets/images/services/Managed Cloud Services.png";
